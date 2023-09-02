445055061234699833a53fce2b63a6876842bfdb
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-09-03 02:25:59 +0530

    Added comments for future reference

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 
+// install json using : npm i json-server -g ( don't run again if run once already )
+// and then run this command to run the api : json-server --watch --port 4000 ./_data/db.json
+
 async function getTickets() {
   const res = await fetch("http://localhost:4000/tickets", {
     // set-up our own api by using data from db.json file . The program will never run properly if this file is not running on another localhost because this acts the source api for the data being used by our program.
