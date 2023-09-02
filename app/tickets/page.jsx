b6920fce419b4f9655c7f73b86e482ead017f36e
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-09-03 02:15:47 +0530

    Added create new ticket button

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -5,12 +5,22 @@ import { Suspense } from "react";
 export default function Tickets() {
   return (
     <main>
-      <nav>
+      <nav style={{ marginBottom: 0 }}>
         <div>
           <h2>Tickets</h2>
           <p>
             <small>Currently open tickets.</small>
           </p>
+          <form
+            action="/tickets/create"
+            style={{
+              backgroundColor: "#ebf0fa",
+              paddingLeft: 0,
+              paddingBottom: 0,
+            }}
+          >
+            <button className="btn-primary">Create New Ticket</button>
+          </form>
         </div>
       </nav>
 
